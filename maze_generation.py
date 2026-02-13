b3daac8a54beec6a1d8ea9797d892cb0858f0056
iser-80 <issamerrafiy01@gmail.com>
2026-02-13 13:31:58 +0100

    add maze_generation

---
+++ maze_generation.py
@@ -0,0 +1,59 @@
+import random
+import Parsing
+
+
+def break_wall(grid, x1, y1, x2, y2):
+	dx = x1 - x2
+	dy = y1 - y2
+
+	if dy == -1:
+		grid[y1][x1] -= 1
+		grid[y2][x2] -= 4
+	elif dx == 1:
+		grid[y1][x1] -= 2
+		grid[y2][x2] -= 8
+	elif dy == 1:
+		grid[y1][x1] -= 4
+		grid[y2][x2] -= 1
+	elif dx == -1:
+		grid[y1][x1] -= 8
+		grid[y2][x2] -= 2
+
+def generate_maze(maze):
+	try:
+		width = maze["WIDTH"]
+		height = maze["HEIGHT"]
+		x, y = maze["ENTRY"]
+
+		stack = [(x, y)]
+		directions = [
+			(0, -1, 1),
+			(1, 0, 2),
+			(0, 1, 4),
+			(-1, 0, 8),
+		]
+		visited = set()
+		visited.add((x, y))
+		grid = [[15 for _ in range(width)] for _ in range(height)]
+
+		while stack:
+			unvisited_cells = []
+			current_x, current_y = stack[-1]
+			
+			for dir_x, dir_y in directions:
+				nx_x, nx_y = current_x + dir_x, current_y + dir_y
+				if 0 < nx_x <= width and 0 < nx_y <= height and (nx_x, nx_y) not in visited:
+					unvisited_cells.append((nx_x, nx_y, dir_x, dir_y))
+			
+			if unvisited_cells:
+				nx_x, nx_y, dir_x, dir_y = random.choice(unvisited_cells)
+
+				break_wall(grid, current_x, current_y, nx_x, nx_y)
+				visited.add((nx_x, nx_y))
+				stack.append((nx_x, nx_y))
+			else:
+				stack.pop()
+	
+		return grid
+	except Exception as e:
+		print(f"Error: an unexpected error occured {e}")
--- parsing.py
+++ parsing.py
@@ -29,15 +29,33 @@ def parse_data(file):
 		print(f"Error: Unexpected error occured {e}")
 		return None
 
-def validate_config_data(config)
-{
+
+def validate_config_data(config):
 	try:
+		entry = config["ENTRY"]
+		exitt = config["EXIT"]
 		width = config["WIDTH"]
-	
+		height = config["HEIGHT"]
+		perfect = config["PERFECT"]
+
+		if width <= 0 or height <= 0:
+			print("Error: Width and height must be positive")
+			return False
+		if not 0 < entry[0] <= width and 0 < entry[1] <= height:
+			print("Error: Invalid entry postion range")
+			return False
+		if not 0 < exitt[0] <= width and 0 < exitt[1] <= height:
+			print("Error: Invalid exit postion range")
+			return False
+		if entry[0] == exitt[0] and entry[1] == exitt[1]:
+			print("Error: entry postion must be diffrent than the exit")
+			return False
+		if width < 5 or height < 3:
+            print("Error: 42 pattern can't be omitted in less than 5x3")
+            return False
+
+		return True
 	except ValueError as e:
 		print(f"Error: Invalid data input {e}")
-}
-
-
-test = parse_data("test.txt")
-print(test)
+	except Exception as e:
+		print(f"Error: An unexpected error occured {e}")
