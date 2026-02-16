6fcaf260aff160f6fa8c4dc89de53f15e9bfd95b
iser-80 <issamerrafiy01@gmail.com>
2026-02-16 14:47:48 +0000

    complete the maze logic

--- parsing.py
+++ parsing.py
@@ -1,33 +1,35 @@
 def parse_data(file):
-	config = {}
-	try:
-		with open(file, "r") as file_data:
-			for line in file_data:
-				line = line.strip()
-				if not line or line.startswith("#"):
-					continue
-				if "=" in line:
-					key, value = line.split("=")
-					key = key.strip()
-					value = value.strip()
-					if key in ["WIDTH", "HEIGHT"]:
-						config[key] = value
-					elif key in ["ENTRY", "EXIT"]:
-						config[key] = [int(item) for item in value.split(",")]
-					elif key == "PERFECT":
-						config[key] = value.lower() == "true"
-					else:
-						config[key] = value
-		return config
-	except FileNotFoundError:
-		print(f"Error: {file} dosen't exist")
-		return None
-	except ValueError as e:
-		print(f"Error: Invalid data processing {e}")
-		return None
-	except Exception as e:
-		print(f"Error: Unexpected error occured {e}")
-		return None
+    config = {}
+    try:
+        with open(file, "r") as file_data:
+            for line in file_data:
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                if "=" in line:
+                    key, value = line.split("=")
+                    key = key.strip()
+                    value = value.strip()
+                    if key in ["WIDTH", "HEIGHT"]:
+                        config[key] = int(value)
+                    elif key in ["ENTRY", "EXIT"]:
+                        config[key] = [int(item) for item in value.split(",")]
+                    elif key == "PERFECT":
+                        config[key] = value.lower() == "true"
+                    elif key == "SEED":
+                        config[key] = int(value)
+                    else:
+                        config[key] = value
+        return config
+    except FileNotFoundError:
+        print(f"Error: {file} dosen't exist")
+        return None
+    except ValueError as e:
+        print(f"Error: Invalid data processing {e}")
+        return None
+    except Exception as e:
+        print(f"Error: Unexpected error occured {e}")
+        return None
 
 
 def validate_config_data(config):
@@ -36,6 +38,7 @@ def validate_config_data(config):
         height = int(config["HEIGHT"])
         entry_x, entry_y = config["ENTRY"]
         exit_x, exit_y = config["EXIT"]
+        seed = config["SEED"]
 
         if width <= 0 or height <= 0:
             print("Error: Width and height must be positive")
@@ -55,6 +58,10 @@ def validate_config_data(config):
 
         if width < 5 or height < 3:
             print("Error: 42 pattern can't be omitted in less than 5x3")
+            return False
+
+        if seed != 42:
+            print("Error: the seed should be 42")
             return False
 
         return True
--- solve_maze.py
+++ solve_maze.py
@@ -1,73 +1,4 @@
-import parsing
-import maze_generation
-
 from collections import deque
-
-
-def solve_maze(grid, entry, exit):
-	width = len(grid[0])
-	height = len(grid)
-	start = tuple(entry)
-	end = tuple(exit)
-
-	directions = [
-        (0, -1, 1, 'N'),
-        (1, 0, 2, 'E'),
-        (0, 1, 4, 'S'),
-        (-1, 0, 8, 'W'),
-    ]
-	queue = deque([start])
-	paths = {start: None}
-
-	while queue:
-		cur_x, cur_y = queue.popleft()
-		if (cur_x, cur_y) == end:
-			return reconstruct_path(paths, start, end)
-		
-		for dir_x, dir_y, bit, char in directions:
-			nx_x, nx_y = cur_x + dir_x, cur_y + dir_y
-			if 0 <= nx_x < width and 0 <= nx_y < height:
-				if not (grid[cur_y][cur_x] & bit) and (nx_x, nx_y) not in paths:
-					paths[(nx_x, nx_y)] = ((cur_x, cur_y), char)
-					queue.append((nx_x, nx_y))
-
-	return "path not found"
-
-def reconstruct_path(paths, start, end):
-	current = end
-	path_letter = []
-
-	while current != start:
-		(prev_x, prev_y), dir_char = paths[current]
-		path_letter.append(dir_char)
-		current = (prev_x, prev_y)
-
-	return "".join(reversed(path_letter))
-
-
-# TESTING
-def debug_draw_maze(grid):
-    width = len(grid[0])
-    # Print top border
-    print("+" + "---+" * width)
-    
-    for row in grid:
-        # 1. Print the horizontal paths (West/East)
-        row_str = "|"
-        for cell in row:
-            # If bit 2 (EAST) is NOT set, there is a path (space)
-            # If it IS set, there is a wall (|)
-            wall = " " if not (cell & 2) else "|"
-            row_str += "   " + wall
-        print(row_str)
-        
-        # 2. Print the vertical paths (North/South)
-        bottom_str = "+"
-        for cell in row:
-            # If bit 4 (SOUTH) is NOT set, there is a path (space)
-            wall = "   " if not (cell & 4) else "---"
-            bottom_str += wall + "+"
-        print(bottom_str)
 
 def export_maze(config, grid, path_string=""):
     output_file = config.get("OUTPUT_FILE", "maze.txt")
@@ -86,12 +17,3 @@ def export_maze(config, grid, path_string=""):
         
         f.write(path_string + "\n")
         print(f"Successfully exported maze to {output_file}")
-
-if __name__ == "__main__":
-    config = parsing.parse_data("test.txt")
-    if config:
-        grid = maze_generation.generate_maze(config)
-        
-        print("\n--- DEBUG MAZE VIEW ---")
-        debug_draw_maze(grid)
-        export_maze(config, grid, path_string="PATH_PENDING_BFS")
